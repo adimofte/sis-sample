417ec3287a7745f09d248b0499802ad3349f5522
Andu Dimofte <andu.dimofte@snowflake.com>
2024-09-11 09:51:21 +0000

    gg

--- pages/1_page2.py
+++ pages/1_page2.py
@@ -17,7 +17,7 @@ session = get_active_session()
 
 # Use an interactive slider to get user input
 hifives_val = st.slider(
-  "Number of high-fives in Q",
+  "Number of high-fives in Q3",
   min_value=0,
   max_value=90,
   value=60,
@@ -29,7 +29,7 @@ hifives_val = st.slider(
 #  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
 created_dataframe = session.create_dataframe(
   [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
-  schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTERS"],
+  schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
 )
 
 # Execute the query and convert it into a Pandas dataframe
@@ -37,8 +37,8 @@ queried_data = created_dataframe.to_pandas()
 
 # Create a simple bar chart
 # See docs.streamlit.io for more types of charts
-st.subheader("# of high-fives")
-st.bar_chart(data=queried_data, x="QUARTERS", y="HIGH_FIVES")
+st.subheader("Number of high-fives")
+st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
 
 st.subheader("Underlying data")
 st.dataframe(queried_data, use_container_width=True)
